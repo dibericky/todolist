fb5dafbe523b2a19f0af88573bfeff59e0dc731d
dibericky <dibe.ricky@gmail.com>
2018-12-27 21:19:34 +0100

    minor fix

--- api/config/core.php
+++ api/config/core.php
@@ -2,9 +2,6 @@
 // show error reporting
 error_reporting(E_ALL);
  
-// set your default time-zone
-date_default_timezone_set('Asia/Manila');
- 
 // variables used for jwt
 $root = $_SERVER['DOCUMENT_ROOT'];
 $backend = $root.'api';
